deb5502de18863f44d0eb0823f244db71a2fc55d
Jithin-Ramachandran <jithin.ramachandran@resemblesystems.com>
2025-01-07 12:51:57 +0545

    User & Role Created

---
+++ vendor/src/main/java/com/vendor/entity/Role.java
@@ -0,0 +1,14 @@
+package com.vendor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
